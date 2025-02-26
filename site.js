4e37c0ea85b4f826355503696fa810d55cda6bde
Ryan Appel <rdappel@gmail.com>
2025-02-26 00:20:20 -0600

    moved game info into html

--- site.js
+++ site.js
@@ -1,6 +1,4 @@
 
-const footerYear = document.querySelector('footer .current')
-footerYear.textContent = new Date().getFullYear()
 
 const heroContent = document.querySelector('#hero-content')
 document.addEventListener('scroll', () => {
@@ -8,67 +6,20 @@ document.addEventListener('scroll', () => {
 	heroContent.style.transform = `translateY(-${window.scrollY / 2}px)`
 })
 
-const setupNavLinks = () => {
-	const navHeight = document.querySelector('nav').offsetHeight
-	const navLinks = document.querySelectorAll('nav a')
-	navLinks.forEach(link => {
-		link.addEventListener('click', e => {
-			e.preventDefault()
-			const target = document.querySelector(link.getAttribute('href'))
-			const position = target.getBoundingClientRect().top + window.scrollY - navHeight
-			const top = (position < 0 ? 0 : position)
-			const behavior = 'smooth'
-			console.log(position)
-			
-			window.scrollTo({ top, behavior })
-			//target.scrollIntoView({ behavior: 'smooth' })
-		})
+const footerYear = document.querySelector('footer .current')
+footerYear.textContent = new Date().getFullYear()
+
+const navHeight = document.querySelector('nav').offsetHeight
+const navLinks = document.querySelectorAll('nav ul a')
+navLinks.forEach(link => {
+	link.addEventListener('click', e => {
+		e.preventDefault()
+		const target = document.querySelector(link.getAttribute('href'))
+		const position = target.getBoundingClientRect().top + window.scrollY - navHeight
+		const top = (position < 0 ? 0 : position)
+		const behavior = 'smooth'
+		console.log(position)
+		
+		window.scrollTo({ top, behavior })
 	})
-}
-
-// generate games
-
-const fetchGameIds = async () => {
-	const response = await fetch('/data/game-list.json')
-	return await response.json()
-}
-
-const fetchGameData = async id => {
-	const response = await fetch(`/data/${id}.json`)
-	return await response.json()
-}
-
-const generateGameElement = async id => {
-	const gameData = await fetchGameData(id)
-
-	const {title, imageUrl, description} = gameData
-
-	const gameElement = document.createElement('div')
-	gameElement.classList.add('game')
-
-	const imageElement = document.createElement('img')
-	imageElement.src = `/images/games/${imageUrl}`
-	imageElement.alt = title
-	gameElement.appendChild(imageElement)
-
-	const titleElement = document.createElement('h2')
-	titleElement.textContent = title
-	gameElement.appendChild(titleElement)
-
-	const descriptionElement = document.createElement('p')
-	descriptionElement.textContent = description
-	gameElement.appendChild(descriptionElement)
-
-	return gameElement
-}
-
-
-(async () => {
-	const gameIds = await fetchGameIds()
-	const promises = gameIds.map(id => generateGameElement(id))
-	const gameElements = await Promise.all(promises)
-	const gamesElement = document.querySelector('#games')
-	gameElements.forEach(element => gamesElement.appendChild(element))
-
-	setupNavLinks()	
-})()
+})
